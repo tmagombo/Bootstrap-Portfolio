d55eaa1fdb178a6af6e36fc9ffb1218e157bdd1f
Tristan <magombotristan@gmail.com>
2019-03-30 19:17:54 -0700

    New style with added projects and contact info

---
+++ app.js
@@ -0,0 +1,14 @@
+// $(".phone").click(function(){
+//     $(".phone1").empty();
+//     $(".phone").text("Phone Number: 520-595-6049")
+// });
+
+// $(".email").click(function(){
+//     $(".email1").empty();
+//     $(".email").text("Email: magombotristan@gmail.com")
+// });
+
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
